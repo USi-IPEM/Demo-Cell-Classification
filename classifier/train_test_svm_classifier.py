c0183c7a9409885080919eb0c75e03d59d4b24a9
moritz wolter <moritz.wolter@scai.fraunhofer.de>
2020-10-30 15:10:39 +0100

    added mlp and tree models.

--- classifier/train_test_svm_classifier.py
+++ classifier/train_test_svm_classifier.py
@@ -2,6 +2,8 @@
 import os
 import numpy as np
 from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 from data_loader import VectorLoader
 
 
@@ -14,16 +16,39 @@ path_lst = ['./01_Data/201027/use_case2/Processed/Samples/',
 
 demo_cell_data = VectorLoader(case_path_lst=path_lst, debug=False)
 X_train, y_train = demo_cell_data.get_train_xy()
-
-clf = svm.SVC(gamma='auto')
-clf.fit(X_train, y_train.ravel())
-
 X_test, y_test = demo_cell_data.get_test_xy()
-
-svm_out = clf.predict(X_test)
+### SVM --------------------------------------------------------------###
+svm = svm.SVC(gamma='auto')
+svm.fit(X_train, y_train.ravel())
+svm_out = svm.predict(X_test)
 ## compute accuracy
 y_test = np.squeeze(y_test)
-print('svm_out', svm_out)
-print('y test', y_test)
-print('svm_out == y_test', svm_out == y_test)
-print('accuracy:', np.sum((svm_out == y_test).astype(np.float32))/svm_out.shape[0]*100)
+print('sample svm_out    ', svm_out.astype(np.uint8))
+print('sample y test     ', y_test.astype(np.uint8))
+print('svm_out == y_test ', (svm_out == y_test).astype(np.uint8))
+print('SVM accuracy:',
+      np.sum((svm_out == y_test).astype(np.float32))/svm_out.shape[0]*100)
+
+### MLP ---------------------------------------------------------------###
+mlp = MLPClassifier(max_iter=2000)
+mlp.fit(X_train, y_train.ravel())
+mlp_out = mlp.predict(X_test)
+## compute accuracy
+y_test = np.squeeze(y_test)
+print('sample mlp_out    ', mlp_out.astype(np.uint8))
+print('sample y test     ', y_test.astype(np.uint8))
+print('mlp_out == y_test ', (mlp_out == y_test).astype(np.uint8))
+print('MLP accuracy:',
+      np.sum((mlp_out == y_test).astype(np.float32))/mlp_out.shape[0]*100)
+
+### Tree --------------------------------------------------------------###
+tree = DecisionTreeClassifier()
+tree.fit(X_train, y_train.ravel())
+tree_out = tree.predict(X_test)
+## compute accuracy
+y_test = np.squeeze(y_test)
+print('sample tree_out   ', tree_out.astype(np.uint8))
+print('sample y test     ', y_test.astype(np.uint8))
+print('tree_out == y_test', (tree_out == y_test).astype(np.uint8))
+print('tree accuracy:',
+      np.sum((tree_out == y_test).astype(np.float32))/tree_out.shape[0]*100)
